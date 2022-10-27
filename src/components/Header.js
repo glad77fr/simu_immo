e60564e85d99cc49901e42b91212fd348575c6d8
sabrigasmi <glad77fr @yahoo.fr>
2022-10-27 16:39:05 +0200

    V0

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import "../styles/nav.css"
+
+function Header() {
+return (
+    <nav class="navbar navbar-dark bg-dark">
+        <div class='nav_item'> Simu immo </div>
+    </nav>
+)
+}
+export default Header
